138d4e69dc6a1108c3bc548c28da25c6844386a5
m00g <m00g@sodecent.ee>
2026-04-06 01:34:36 +0300

    feat(echo): dynamically set wshost for universal development and production environments

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,7 +6,12 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
+    // Dynamically get the browser's current hostname for local development
+    // but allow overriding via standard Reverb env vars for production
+    wsHost: import.meta.env.VITE_REVERB_HOST === 'localhost'
+        || import.meta.env.VITE_REVERB_HOST?.startsWith('http') 
+        ? window.location.hostname 
+        : import.meta.env.VITE_REVERB_HOST,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
